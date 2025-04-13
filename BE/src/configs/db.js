a06f0e4efd3c6328593278d18e48c60c9d5bd0fe
quach thuan minh triet <109292564+quachthuanminhtriet@users.noreply.github.com>
2025-04-13 08:30:50 +0700

    update

--- BE/src/configs/db.js
+++ BE/src/configs/db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
+    process.env.DB_NAME  || process.env.DB_NAME_RAILWAY,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PASSWORD || process.env.DB_PASSWORD_RAILWAY,
     {
-        host: process.env.DB_HOST,
+        host: process.env.DB_HOST  || process.env.DB_HOST_RAILWAY,
         dialect: 'mysql',
         logging: false,
         define: {
